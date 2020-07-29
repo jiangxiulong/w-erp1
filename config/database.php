20c4cb68f788113778e467ce75668fffba89226c
jxl <jxl@xiulong.top>
2020-07-29 15:49:49 +0800

    '登录界面去掉版权显示，退出位置去掉没用的'

--- config/database.php
+++ config/database.php
@@ -21,7 +21,7 @@ return [
     // 服务器地址
     'hostname'    => '127.0.0.1',
     // 数据库名
-    'database'    => 'admin_v5',
+    'database'    => 'erp1',
     // 用户名
     'username'    => 'root',
     // 密码
